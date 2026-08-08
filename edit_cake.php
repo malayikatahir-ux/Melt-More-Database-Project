2673814debf41bf15d51606dc6014cfb67c817d9
Malayika Tahir <malayikatahir@gmail.com>
2026-08-08 07:35:04 +0500

    Delete edit_cake.php

--- edit_cake.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'auth.php';
-require_once '../db.php';
-$id = intval($_GET['id'] ?? 0);
-if ($id) $conn->query("DELETE FROM ingredients WHERE id=$id");
-header('Location: ingredients.php?deleted=1');
-exit();
-?>
